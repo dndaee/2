ca282565f5cc476d8f9c44a8a1f2db417671ef7b
dndaee <vespa.faber@gmail.com>
2025-06-09 11:45:45 +0300

    Create 1_2.py

---
+++ 1_2.py
@@ -0,0 +1,12 @@
+A = float(input("Введіть щомісячну стипендію (грн): "))
+B = float(input("Введіть щомісячні витрати на початку (грн): "))
+
+total_needed_from_parents = 0
+current_expenses = B
+
+for month in range(1, 11):
+    shortage = current_expenses - A
+    total_needed_from_parents += shortage
+    current_expenses *= 1.05  # Збільшення витрат на 5%
+
+print(f"Сума, яку потрібно попросити в батьків: {total_needed_from_parents:.2f} грн")
